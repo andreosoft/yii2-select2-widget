bfbf3fdac8e107087ef7f944b7c0f0a0e780ec95
andreosoft <AndreyZagorets@gmail.com>
2017-04-29 15:48:07 +0300

    Update Asset.php

--- Asset.php
+++ Asset.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace andreosoft\select2;
 
 use yii\web\AssetBundle;
@@ -6,11 +7,12 @@ use yii\web\AssetBundle;
 class Asset extends AssetBundle {
 
     public $sourcePath = '@bower/select2';
-    public $js = [
-        'js/select2.min.js',
-        'css/select2.min.css',
+    public $css = [
+        'dist/css/select2.min.css',
     ];
-
+    public $js = [
+        'dist/js/select2.min.js',
+    ];
     public $depends = [
         'yii\web\JqueryAsset',
     ];

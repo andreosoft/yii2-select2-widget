5882adb1757262a26e77ef591f0642572aeaa1e9
andreosoft <AndreyZagorets@gmail.com>
2017-04-29 15:48:41 +0300

    Update Select2.php

--- Select2.php
+++ Select2.php
@@ -9,6 +9,7 @@ use yii\helpers\Url;
 
 class Select2 extends InputWidget {
 
+    public $items = [];
     public $widgetOptions = [];
     public $contentsCss = [];
 
@@ -18,16 +19,16 @@ class Select2 extends InputWidget {
         $id = Json::encode('#' . $this->options['id']);
         $view = $this->getView();
         $widgetOptions = empty($this->widgetOptions) ? '' : ', ' . (Json::encode($this->widgetOptions));
-        $js = "$('id').select2($widgetOptions);";
+        $js = "$($id).select2($widgetOptions);";
         $view->registerJS($js);
         Asset::register($view);
     }
 
     public function run() {
         if ($this->hasModel()) {
-            echo Html::activeDropDownList($this->model, $this->attribute, $this->options);
+            echo Html::activeDropDownList($this->model, $this->attribute, $this->items, $this->options);
         } else {
-            echo Html::dropDownList($this->name, $this->value, $this->options);
+            echo Html::dropDownList($this->name, $this->value, $this->items, $this->options);
         }
     }
 
